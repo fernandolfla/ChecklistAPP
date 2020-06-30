a299c112a1cf59aef5c40f360b22a4e8e8c85ecd
luiz fernando lima avelar <fernandolfla@gmail.com>
2020-06-29 21:33:24 -0300

    Calibrado interface

--- ChecklistAPP/ChecklistAPP.Android/Properties/AssemblyInfo.cs
+++ ChecklistAPP/ChecklistAPP.Android/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.20.4.0")]
+[assembly: AssemblyVersion("1.20.19.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
